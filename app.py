90a8eeda4e0bb4a9332e88a8e2692bd6e9e54463
ascd1989 <73740808+ascd1989@users.noreply.github.com>
2022-01-12 21:48:43 +0530

    make it better

--- app.py
+++ app.py
@@ -137,4 +137,5 @@ def uploaded_file(filename):
                                filename)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=80)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port,debug=True)
